c532ac6fc26af1e3a374b61ca4339ba34c026fb3
satrioo <satrioari43@gmail.com>
2024-05-09 14:51:22 +0700

    first commit

---
+++ src/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div ref="viewer" id="viewer" style="width: 100vw; height: 100vh"></div>
+</template>
+
+<script>
+import { Viewer } from "@photo-sphere-viewer/core";
+import '@photo-sphere-viewer/core/index.css'
+
+export default {
+  name: 'PhotoSphereViewer',
+  mounted() {
+    new Viewer({
+      container: "viewer",
+      panorama: 'https://i.ibb.co/2sWtDGQ/bb.jpg',
+      caption: "Preview Photo",
+      touchmoveTwoFingers: true,
+      mousewheelCtrlKey: true,
+      defaultZoomLvl: 0
+    });
+  },
+};
+</script>
